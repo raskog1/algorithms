267a9b428f20e440cf64630a90884cb3cc5b1f05
Ryan Skog <ryan.skog@outlook.com>
2020-10-08 19:33:05 -0700

    First commit, all unsolved algorithms

---
+++ 1/01-log-even-nums/Unsolved/log-even-nums.js
@@ -0,0 +1,12 @@
+// Write code to print all even numbers from 0 to `num`
+// Assume `num` will be a positive number
+
+var logEvenNums = function(num) {
+  let i = 0;
+  while (i <= num) {
+    console.log(i);
+    i += 2;
+  }
+};
+
+// SOLVED 8.13.20
---
+++ 1/02-countdown/Unsolved/countdown.js
@@ -0,0 +1,12 @@
+// Write code to print all numbers from `num` down to 1
+// You may assume `num` will be a positive number
+
+var countdown = function(num) {
+  let i = num;
+  while (i >= 1) {
+    console.log(i);
+    i--;
+  }
+};
+
+// SOLVED 8.13.20
